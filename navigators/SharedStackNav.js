9a0222f7fa3a4ee3502bf501b1613a2fd14da087
지혜인(Hyein Ji)/Media Integration팀/SKP <hyein.ji@sk.com>
2021-06-14 18:19:34 +0900

    15.8 Photo part Three

--- navigators/SharedStackNav.js
+++ navigators/SharedStackNav.js
@@ -7,6 +7,8 @@ import Feed from "../screens/Feed";
 import Search from "../screens/Search";
 import Notifications from "../screens/Notifications";
 import Me from "../screens/Me";
+import Likes from "../screens/Likes";
+import Comments from "../screens/Comments";
 
 const Stack = createStackNavigator();
 
@@ -50,6 +52,8 @@ export default function SharedStackNav({ screenName }) {
       {screenName === "Me" ? <Stack.Screen name="Me" component={Me} /> : null}
       <Stack.Screen name="Profile" component={Profile} />
       <Stack.Screen name="Photo" component={Photo} />
+      <Stack.Screen name="Likes" component={Likes} />
+      <Stack.Screen name="Comments" component={Comments} />
     </Stack.Navigator>
   );
 }
