08ba0c84a9aaba4023639089fec4b59e38a4ac3a
지혜인(Hyein Ji)/Media Integration팀/SKP <hyein.ji@sk.com>
2021-06-16 14:51:22 +0900

    +logout link

--- screens/Me.js
+++ screens/Me.js
@@ -1,6 +1,16 @@
 import React, { useEffect } from "react";
 import { Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import styled from "styled-components/native";
 import useMe from "../hooks/useMe";
+import { logUserOut } from "../apollo";
+import { colors } from "../colors";
+
+const LogoutText = styled.Text`
+  color: ${colors.blue};
+  font-size: 16px;
+  font-weight: 600;
+`;
 
 export default function Me({ navigation }) {
   const { data } = useMe();
@@ -19,6 +29,9 @@ export default function Me({ navigation }) {
       }}
     >
       <Text style={{ color: "white" }}>Me</Text>
+      <TouchableOpacity onPress={logUserOut}>
+        <LogoutText>Logout</LogoutText>
+      </TouchableOpacity>
     </View>
   );
 }
